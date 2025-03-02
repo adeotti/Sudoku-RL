394c547ccb35c68faf9ee4f361c6dc4b6b22ae01
Wafick <wafickachiambi@gmail.com>
2025-03-02 09:49:38 -0600

    Update

--- test.py
+++ test.py
@@ -7,56 +7,74 @@ from PySide6.QtCore import QTimer
 import torch
 import torch.nn.functional as F
 import torch.nn as nn
+ 
 
 app = QApplication.instance()
 if app is None:
     app = QApplication()
 
-class ActorNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.size = 81
-        self.outputShape = 27 # 3*9 = 27 haha
-        self.outputReshaped = (3,9)
-    
-
-        self.input_layer = nn.LazyLinear(81)
-        self.flat = nn.Flatten()
-        self.dense_one = nn.LazyLinear(self.size)
-        self.dense_two = nn.LazyLinear(self.size)
-        self.output = nn.LazyLinear(self.outputShape)
-
-    def forward(self,x):
-        x = self.flat(x)
-        x = F.relu(self.input_layer(x))
-        x = F.relu(self.dense_one(x))
-        x = F.relu(self.dense_two(x))
-        x = F.relu(self.output(x))
-        x = torch.unflatten(x,-1,(self.outputReshaped))
-        return F.softmax(x,-1)
-    
-    def init(self):
-        inittensor = torch.rand([1,3,9]) 
-        self.forward(inittensor)
-        return self
-         
-Actor = ActorNetwork() 
-Actor.forward(torch.rand([1,3,9]))
-Actor.load_state_dict(torch.load("trainingData/new/actor_100k.pth"))
-
-                   
-
  
 
+x = torch.rand(size=(1,9,9))
+ 
+@torch.no_grad()
+def weights_init(w):
+  if isinstance(w,(nn.Conv2d,nn.LazyConv2d,nn.LazyLinear)):
+    nn.init.kaiming_uniform(w.weight,mode="fan_in",nonlinearity="relu")
+    if w.bias is not None : nn.init.zeros_(w.bias)
+
+def networkInit(network : nn.Module):
+  network.forward(x)
+  network.apply(weights_init)
+  return network
+
+class Mask: 
+  # This will alter the softmax distribution so value in [x,y,value] != 0 
+  def __init__(self):
+    self.newValue = -float("inf")
+
+  def apply(self,tensor : torch.FloatTensor):
+    self.mask = torch.zeros_like(tensor,dtype=torch.bool)
+    self.mask[-1,-1,0] = True
+    tensor = tensor.masked_fill(mask=self.mask,value=self.newValue)
+    return tensor
+
+
+class ActorNetwork(nn.Module):
+  def __init__(self):
+    super().__init__()
+    self.size = 81
+    self.outputShape = 27 # 3*9 = 27 haha
+    self.outputReshaped = (3,9)
+    self.mask = Mask()
+
+    self.input_layer = nn.LazyLinear(81)
+    self.flat = nn.Flatten()
+    self.dense_one = nn.LazyLinear(self.size)
+    self.dense_two = nn.LazyLinear(self.size)
+    self.output = nn.LazyLinear(self.outputShape)
+
+  def forward(self,x):
+    x = self.flat(x)
+    x = F.relu(self.input_layer(x))
+    x = F.relu(self.dense_one(x))
+    x = F.relu(self.dense_two(x))
+    x = F.relu(self.output(x))
+    x = torch.unflatten(x,-1,(self.outputReshaped))
+    x = self.mask.apply(x)
+    return F.softmax(x,-1)
+         
+Actor = networkInit(ActorNetwork())
+Actor.load_state_dict(torch.load("./actor_100k.pth"),strict=False)
+
 env = gymnasium.make("sudoku")
-sys.exit( )
 
 def format_observation(env = env):
     observation = env.reset()[0]
     observation = torch.tensor(observation).float().unsqueeze(0)
     return observation
 
-def action_generator(actor = actor):
+def action_generator(actor = Actor):
     obs = format_observation()
     action = actor(obs)
     action = Categorical(action).sample()[0].tolist()
@@ -98,5 +116,4 @@ class Test:
      
 test = Test(render=True) # Setting render to false will lead to faster computing obviously.
 test.run()
-
-
+ 
